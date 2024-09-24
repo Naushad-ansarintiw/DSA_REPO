e93c23e80999afbeebe995552837867daff404f7
Mohd Naushad <113180514+Naushad-ansarintiw@users.noreply.github.com>
2024-09-24 22:49:46 +0530

    Create trie-implemantation-codestorywithmik.cpp

---
+++ trie-implemantation-codestorywithmik.cpp
@@ -0,0 +1,34 @@
+// Trie Implementation with codewithmik
+struct trieNode {
+  bool endOfWord;
+  string word;
+  trieNode* children[26];
+};
+
+trieNode* getNode() {
+  trieNode* newNode = new trieNode();
+  newNode->endOfWord = false;
+  newNode->word = "";
+
+  for(int i=0; i<26; i++) {
+      newNode->children[i] = NULL;
+  }
+}
+
+void insert(trieNode* root, string& word) {
+  trieNode* crawler = root;
+
+  // check for each character of word
+  for(int i=0; i<word.length(); i++) {
+      char ch = word[i];  
+
+      if(crawler->children[ch-'a'] == NULL) {
+          crawler->children[ch-'a'] = getNode();
+      }
+      crawler = crawler->children[ch-'a'];
+  }
+
+  crawler->endOfWord = true;
+  crawler->word = word;
+}
+  
